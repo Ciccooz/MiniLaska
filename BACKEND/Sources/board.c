61e7e1dec45ea87d9ca694c2ec162af684e62e89
Nicola Marizza <nicola.marizza@gmail.com>
2020-11-15 14:54:28 +0100

    Added allocation exception handling

--- BACKEND/Sources/board.c
+++ BACKEND/Sources/board.c
@@ -3,26 +3,56 @@
 static PlayableBoard allocBoard();
 static void spawnPawns(PlayableBoard grid);
 
-
+/*
+Alloca la memoria per il campo da gioco tramite la chiamata ad allocBoard e posiziona le pedine
+nella posizione di inizio partita tramite la chiamata a spawnPawns
+Restituisce il puntatore base (PlayableBoard) o NULL in caso si verificasse un'eccezione durante
+l'allocazione di memoria
+*/
 PlayableBoard newBoard()
 {
    PlayableBoard board = allocBoard();
+   if(board == NULL)
+      return NULL;
+
    spawnPawns(board);
    return board;
 }
 
+/*
+Alloca dinamicamente la memoria per il campo da gioco (limitato alle caselle giocabili) e
+restituisce il puntatore base (PlayableBoard)
+Se la calloc non riesce ad allocare memoria la funzione ripulisce tutto ciò che eventualmente
+era già stato allocato prima che si verificasse l'eccezione chiamando freeBoard e restituisce
+NULL
+*/
 PlayableBoard allocBoard()
 {
-   PlayableBoard board = (PlayableBoard) malloc(GRID_SIZE * sizeof(Tower*));
-
    int row, col;
+   PlayableBoard board = (PlayableBoard) calloc(GRID_SIZE, sizeof(Tower*));
+   if(board == NULL)
+      return NULL;
+
    for(row = 0; row < GRID_SIZE; row++)
    {
       int rowSize = getRowSize(row);
 
-      board[row] = (Tower*) malloc(rowSize * sizeof(Tower));
+      board[row] = (Tower*) calloc(rowSize, sizeof(Tower));
+      if(board[row] == NULL)
+      {
+         freeBoard(board);
+         return NULL;
+      }
+
       for(col = 0; col < rowSize; col++)
-         board[row][col] = (Tower) malloc(TOWER_HEIGHT * sizeof(Pawn));
+      {
+         board[row][col] = (Tower) calloc(TOWER_HEIGHT, sizeof(Pawn));
+         if(board[row][col] == NULL)
+         {
+            freeBoard(board);
+            return NULL;
+         }
+      }
    }
 
    return board;
@@ -53,12 +83,25 @@ int getRowSize(int row)
    return 4;
 }
 
+/*
+Libera la memoria allocata per il campo da gioco. Nel caso dovesse trovare un puntatore NULL
+procede direttamente con la free del buffer in cui tale puntatore è memorizzato.
+*/
 void freeBoard(PlayableBoard board)
 {
    int row, col;
+
+   if(board == NULL)
+      return;
+
    for(row = 0; row < GRID_SIZE; row++)
    {
-      int rowSize = getRowSize(row);
+      int rowSize;
+
+      if(board[row] == NULL)
+         break;
+
+      rowSize = getRowSize(row);
       for(col = 0; col < rowSize; col++)
          free(board[row][col]);
 
