c98bd56f2a0ed18d47e927767639201613325c91
Nicola Marizza <nicola.marizza@gmail.com>
2020-11-30 16:37:03 +0100

    Started UI

---
+++ UI/Headers/UI.h
@@ -0,0 +1,6 @@
+#include "..\\..\\BACKEND\\Headers\\board.h"
+
+/*Size indica quanti char dovrebbe misurare un lato della board*/
+void printBoard(PlayableBoard board, int size);
+
+void printMessage(char* message);
