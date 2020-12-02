3b41c490df2251223774da82ccd00df34e240be0
Mattia <mattia.cicco.mc@gmail.com>
2020-12-02 11:07:33 +0100

    roba

--- BACKEND/Headers/board.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "rules.h"
-#include <stdlib.h> /*for malloc/free*/
-#include <string.h> /*for memset*/
-
-PlayableBoard newBoard();
-void freeBoard(PlayableBoard grid);
-int getRowSize(int row);
